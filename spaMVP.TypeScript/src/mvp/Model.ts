0ba66f770e2758fdd232e4d91dcc8acd439e856f
valentin-lozev <valentin.zh.lozev@gmail.com>
2016-09-05 17:12:31 +0300

    Complete new Model typescript version

---
+++ spaMVP.TypeScript/src/mvp/Model.ts
@@ -0,0 +1,86 @@
+﻿namespace spaMVP {
+
+    export let ModelEvents = {
+        Change: 'change',
+        Destroy: 'destroy'
+    };
+
+    /**
+     *  @class spaMVP.Model
+     */
+    export abstract class Model {
+        private listeners: Object = {};
+
+        constructor() {
+        }
+
+        static subclass = subclassFactory;
+
+        /**
+         *  Attaches an event handler to model raised events.
+         *  @param {String} eventType The name of the event.
+         *  @param {Function} handler The event's handler.
+         *  @param {Object} [context] The Handler's context.
+         */
+        on(eventType: string, handler: (data?: any) => void, context?: Object): boolean {
+            if (!eventType) {
+                return false;
+            }
+
+            this.listeners[eventType] = this.listeners[eventType] || [];
+            this.listeners[eventType].push({
+                handler: handler,
+                context: context
+            });
+            return true;
+        }
+
+        /**
+         *  Detaches an event handler.
+         *  @param {String} eventType The name of the event.
+         *  @param {Function} handler The handler which must be detached.
+         *  @param {Object} [context] The Handler's context.
+         */
+        off(eventType: string, handler: (data?: any) => void, context?: Object): boolean {
+            let listeners = this.listeners[eventType] || [];
+            for (let i = 0, len = listeners.length; i < len; i++) {
+                let listener = listeners[i];
+                if (listener.handler === handler &&
+                    listener.context === context) {
+                    listener = listeners[len - 1];
+                    listeners.length--;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /**
+         *  Notifies the listeners attached for specific event.
+         */
+        notify(type: string, data?: any) {
+            if (!Array.isArray(this.listeners[type])) {
+                return;
+            }
+
+            this.listeners[type]
+                .slice(0)
+                .forEach(listener => listener.handler.call(listener.context, data));
+        }
+
+        /**
+         *  Notifies for change event.
+         */
+        change() {
+            this.notify(ModelEvents.Change, this);
+        }
+
+        /**
+         *  Notifies for destroy event.
+         */
+        destroy() {
+            this.notify(ModelEvents.Destroy, this);
+        }
+    }
+}
---
+++ spaMVP.TypeScript/tests/Model-tests.ts
@@ -0,0 +1,144 @@
+﻿/// <reference path="../jasmine.d.ts" />
+/// <chutzpah_reference path="jasmine.js" />
+
+describe('Model', () => {
+
+    class TestModel extends spaMVP.Model {
+        constructor() {
+            super();
+        }
+    }
+
+    beforeAll(() => {
+    });
+
+    beforeEach(() => {
+    });
+
+    it('should not accept listener when event type is invalid string', () => {
+        let model = new TestModel();
+        let spy = jasmine.createSpyObj('spy', ['listener']);
+
+        expect(model.on('', spy.listener)).toBeFalsy();
+        expect(model.on(null, spy.listener)).toBeFalsy();
+        expect(model.on(undefined, spy.listener)).toBeFalsy();
+    });
+
+    it('should accept listener when event type is valid string', () => {
+        let model = new TestModel();
+        let spy = jasmine.createSpyObj('spy', ['listener']);
+
+        expect(model.on('change', spy.listener)).toBeTruthy();
+    });
+
+    it('should notify without data when there is attached listener', () => {
+        let model = new TestModel();
+        let spy = jasmine.createSpyObj('spy', ['listener']);
+        let event = 'something';
+        model.on(event, spy.listener);
+
+        model.notify(event);
+
+        expect(spy.listener).toHaveBeenCalledWith(undefined);
+    });
+
+    it('should notify with data when there is attached listener', () => {
+        let model = new TestModel();
+        let spy = jasmine.createSpyObj('spy', ['listener']);
+        let event = 'something';
+        model.on(event, spy.listener);
+
+        model.notify(event, 8);
+
+        expect(spy.listener).toHaveBeenCalledWith(8);
+    });
+
+    it('should notify when there is attached listener with given context', () => {
+        let model = new TestModel();
+        let context = null;
+        let action = function () { context = this; };
+        let event = 'something';
+        model.on(event, action, model);
+
+        model.notify(event);
+
+        expect(context).toBe(model);
+    });
+
+    it('should notify when having multiple listeners', () => {
+        let model = new TestModel();
+        let spy = jasmine.createSpyObj('spy', ['save', 'remove', 'insert']);
+        let event = 'something';
+        model.on(event, spy.save);
+        model.on(event, spy.remove);
+        model.on(event, spy.insert);
+
+        model.notify(event);
+
+        expect(spy.save).toHaveBeenCalledTimes(1);
+        expect(spy.remove).toHaveBeenCalledTimes(1);
+        expect(spy.insert).toHaveBeenCalledTimes(1);
+    });
+
+    it('should notify for change with specific string and data', () => {
+        let model = new TestModel();
+        let spy = jasmine.createSpyObj('spy', ['handler']);
+        model.on(spaMVP.ModelEvents.Change, spy.handler);
+
+        model.change();
+
+        expect(spy.handler).toHaveBeenCalledWith(model);
+    });
+
+    it('should notify for destroy with specific string and data', () => {
+        let model = new TestModel();
+        let spy = jasmine.createSpyObj('spy', ['handler']);
+        model.on(spaMVP.ModelEvents.Destroy, spy.handler);
+
+        model.destroy();
+
+        expect(spy.handler).toHaveBeenCalledWith(model);
+    });
+
+    it('should return false when try to detach unattached listener', () => {
+        let model = new TestModel();
+
+        let result = model.off(spaMVP.ModelEvents.Change, () => -1);
+
+        expect(result).toBeFalsy();
+    });
+
+    it('should return true when successfully detach a listener', () => {
+        let model = new TestModel();
+        let spy = jasmine.createSpyObj('spy', ['handler']);
+        model.on(spaMVP.ModelEvents.Change, spy.handler);
+
+        let result = model.off(spaMVP.ModelEvents.Change, spy.handler);
+
+        expect(result).toBeTruthy();
+    });
+
+    it('should return true when successfully detach a listener passed with given context', () => {
+        let model = new TestModel();
+        let spy = jasmine.createSpyObj('spy', ['handler']);
+        model.on(spaMVP.ModelEvents.Change, spy.handler, model);
+
+        let falsyResult = model.off(spaMVP.ModelEvents.Change, spy.handler);
+        let result = model.off(spaMVP.ModelEvents.Change, spy.handler, model);
+
+        expect(falsyResult).toBeFalsy();
+        expect(result).toBeTruthy();
+    });
+
+    it('should not invoke already detached listener', () => {
+        let model = new TestModel();
+        let spy = jasmine.createSpyObj('spy', ['handler']);
+        model.on(spaMVP.ModelEvents.Change, spy.handler);
+
+        let result = model.off(spaMVP.ModelEvents.Change, spy.handler);
+        model.change();
+
+        expect(result).toBeTruthy();
+        expect(spy.handler).not.toHaveBeenCalled();
+    });
+});
